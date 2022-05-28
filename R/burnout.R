2e338f1469b25bc618f82b22f3da53f003cba92b
David Earn <earn@math.mcmaster.ca>
2022-05-27 22:41:19 -0700

    package basics

---
+++ R/burnout.R
@@ -0,0 +1,12 @@
+##' burnout: A package for computing infectious disease persistence probabilities
+##'
+##' The burnout package uses analytical approximations to compute the
+##' probabilities of fizzle and burnout in the stochastic SIR epidemic
+##' model.
+##' 
+##' @section The primary functions are ...
+##'
+##' @docType package
+##' @name burnout
+NULL
+##> NULL
