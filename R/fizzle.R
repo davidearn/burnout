d4113dee556a8b32525818f9ea619f71c8c012b3
David Earn <earn@math.mcmaster.ca>
2022-06-02 20:41:35 -0700

    plot_fizzle_time

--- R/fizzle.R
+++ R/fizzle.R
@@ -63,7 +63,8 @@ not_fizzle_prob <- function(R0, k=1) {
 
 ##' Fizzle time
 ##'
-##' Time \eqn{t} for which probability of fizzle after \eqn{t} is \eqn{< \delta}
+##' Time \eqn{t_\delta} for which probability of fizzle after
+##' \eqn{t_\delta} is \eqn{< \delta}
 ##'
 ##' \deqn{
 ##' 	t_{\delta} =  \dfrac{1}{{\cal R}_{0}-1} \ln\left(\dfrac{(1-\delta)^{-\frac{1}{k}}-\frac{1}{{\cal R}_{0}}}{(1-\delta)^{-\frac{1}{k}}-1}\right).
@@ -72,11 +73,50 @@ not_fizzle_prob <- function(R0, k=1) {
 ##' @inheritParams P1_prob
 ##' @inheritParams fizzle_prob
 ##' @param delta probability threshold
+##' @seealso plot_fizzle_time
 ##'
 ##' @export
+##' 
+##' @examples
+##' fizzle_time(R0 = 2, k = 1, delta = 0.01)
+##' plot_fizzle_time()
 ##' 
 fizzle_time <- function(R0, k, delta) {
     tmp <- (1-delta)^(-(1/k))
     fizz.time <- 1/(R0-1) * log((tmp - 1/R0) / (tmp - 1))
     return(fizz.time)
+}
+
+##' Plot fizzle time
+##'
+##' @inheritParams fizzle_time
+##' @param col vector of colours
+##'
+##' @seealso \code{\link{fizzle_time}}
+##'
+##' @export
+##'
+##' @examples
+##' plot_fizzle_time(delta = 0.01)
+##'
+## FIX: this would be better using facet_wrap if there is more than one delta value
+##
+plot_fizzle_time <- function(R0 = exp(seq(log(1.001),log(2),length=1001)),
+                             k = 1:3, delta = 0.1
+                           , col = 1:length(k)
+                             ##col = c("darkred", "darkgreen", "darkblue")
+                           , lwd=2
+                           , log="x"
+                           , ...
+                             ) {
+    plot(R0, fizzle_time(R0,k=k[1],delta=delta), type="l", lwd=lwd, bty="L",
+         las=1, log=log, col=col[1], ylim=c(0,30),
+         xlab = expression(R[0]), ylab = expression(t[delta]), ...)
+    title(main = latex2exp::TeX(sprintf(
+          "Time $t_\\delta$ for which probability of fizzle after $t_\\delta$ is $< \\delta = %g$", delta)))
+    for (ik in 2:length(k)) {
+        lines(R0, fizzle_time(R0,k=k[ik],delta=delta), lwd=2, col=col[ik])
+    }
+    legend("topright", bty="n", title=expression(I[0]),
+           legend = k, col = col, lwd=lwd)
 }
