fe8984507935af47487460f4dc6572637fe7871a
Ben Bolker <bbolker@gmail.com>
2022-09-01 10:09:35 -0400

    new exact/ode code

--- R/x_in_exact_sirr.R
+++ R/x_in_exact_sirr.R
@@ -44,78 +44,56 @@ x_in_exact_scalar <- function(R0, epsilon, I0=1e-6, ...) {
 ##' 
 x_in_exact <- Vectorize(x_in_exact_scalar)
 
-## epsilon: mean generation interval as fraction of mean lifetime
-## R0
+
 ## -> mean lifetime = L = 1/mu (= 1, nondimensional?)
 ## -> mean generation interval = epsilon*L = 1/gamma
 ## -> beta/(gamma+mu) = R0 -> beta = R0*(gamma+mu) = R0*(1/epsilon +1)
-sirgrad <- function(tau, vars, parms) {
-    with(as.list(c(parms, vars)), {
-        ## more efficient to precompute these ...
-        mu <- 1
-        gamma <- 1/epsilon
-        beta <- R0*(gamma+mu)
-        I <- exp(logI)
+## 1-1/R0 = I*(1/epsilon + 1)
+## I = (1-1/R0)/(1/epsilon + 1) = (1-1/R0)*epsilon/(1+epsilon)
+
+##' @export
+x_in_exact_scalar2 <- function(R0, epsilon, I0=1e-6, tmax = NULL,
+                               nt = 100,
+                               return_traj = FALSE) {
+
+    if (is.null(tmax)) tmax <- 100/(R0-1)
+
+    ## precompute
+    herdImm <- if (R0 > 1) (1 - 1/R0) else 0
+    ## 1-1/R0 = I*(1/epsilon + 1)
+    ## I = (1-1/R0)/(1/epsilon + 1) = (1-1/R0)*epsilon/(1+epsilon)
+    Ihat <- epsilon/(1+epsilon) * herdImm
+    mu <- 1
+    gamma <- 1/epsilon
+    beta <- R0*(gamma+mu)
+
+    sirgrad <- function(tau, vars, parms) {
+        S <- vars[1]
+        I <- exp(vars[2])
         dS <- mu * (1-S) - beta*S*I
         dlogI <- beta*S - (gamma + mu)
         return(list(c(dS=dS, dlogI=dlogI)))
-  })
-}
-
-## 1-1/R0 = I*(1/epsilon + 1)
-## I = (1-1/R0)/(1/epsilon + 1) = (1-1/R0)*epsilon/(1+epsilon)
-rfunc <- function(tau, vars, parms) {
-    with(as.list(c(parms, vars)), {
-        herdImm <- if (R0 > 1) (1 - 1/R0) else 0
-        Ihat <- epsilon/(1+epsilon) * herdImm
-        return(c(Ieqm = exp(logI) - Ihat))
-    })
-}
-
-if (FALSE) {
-
-    pfun <- function(d, parms = p0) {
-        par(las = 1, bty = "l")
-        plot(logI ~ time, data = as.data.frame(d), type ="l", log = "x")
-        Ihat <- with(as.list(parms), epsilon/(1+epsilon)*(1-1/R0))
-        abline(h = log(Ihat), col = 2)
     }
 
-
-    ## shouldn't eventfun only be called at a root?
-    eventfun <- function(t, x, p) { nroot <<- nroot + 1; return(x) }  ## no-op/unchanged
-    ## 1-1/R0 = I*(1/epsilon + 1)
-    ## I = (1-1/R0)/(1/epsilon + 1) = (1-1/R0)*epsilon/(1+epsilon)
+    eventfun <- function(t, x, p) { nroot <<- nroot + 1; return(x) }
+    ## could also condition eventfun on (I = I^* AND deriv is negative)
     rfunc <- function(tau, vars, parms) {
-        with(as.list(c(parms, vars)), {
-            herdImm <- if (R0 > 1) (1 - 1/R0) else 0
-            Ihat <- epsilon/(1+epsilon) * herdImm
-            Ieqm <- exp(logI) - Ihat
-            Ieqm2 <- if (nroot<2) -1 else Ieqm
-            ## cat(Ieqm, nroot, Ieqm2, "\n")
-            return(c(Ieqm, Ieqm2))
-        })
+        Ieqm <- exp(vars[2]) - Ihat
+        ## note eventfun is always called once initially?
+        Ieqm2 <- if (nroot<2) -1 else Ieqm
+        return(c(Ieqm, Ieqm2))
     }
 
-    I0 <- 1e-6
-    p0 <- c(R0 = 5, epsilon = 0.1)
-    d1 <- deSolve::ode(y = c(S=1-I0, logI = log(I0)),
-                       times = seq(0, 100, length.out = 5001),
-                       func = sirgrad,
-                       parms = p0)
-
-    pfun(d1)
     nroot <- 0
-
-    d2 <- deSolve::lsodar(y = c(S=1-I0, logI = log(I0)),
-                          times = seq(0, 10, length.out = 501),
+    dd <- deSolve::lsodar(y = c(S=1-I0, logI = log(I0)),
+                          ## ?? what should length.out be?
+                          times = seq(0, tmax, length.out = nt),
                           func = sirgrad,
-                          parms = list(R0 = 5, epsilon = 0.1),
+                          parms = list(R0 = R0, epsilon = epsilon),
                           rootfunc = rfunc,
-                          events = list(func = eventfun, root = TRUE, terminalroot = 2))
-    pracma::toc()
-    
-    pfun(d2)
-    tail(d2[,"time"], 1)  ## time of second root
+                          events = list(func = eventfun, root = TRUE,
+                                        terminalroot = 2))
 
+    if (return_traj) return(dd)
+    tail(dd[,"S"], 1)
 }
