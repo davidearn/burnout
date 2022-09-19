46d33fa0a7862f89ed32ff08c4f4cf43dda13b3a
Ben Bolker <bbolker@gmail.com>
2022-09-19 09:56:49 -0400

    more flexible end time in x_exact_sirr

--- R/x_in_exact_sirr.R
+++ R/x_in_exact_sirr.R
@@ -6,20 +6,22 @@
 ##' @param I0 initial prevalence (proportion)
 ##' @param nt number of time steps
 ##' @param tmin minimum integration time
-##' @param tmax integration time (default is max(tmin, 100/(R0-1)))
+##' @param maxit max number of tries to double integration time
+##' @param t_int integration time (default is max(tmin, 100/(R0-1)))
 ##' @param return_traj (logical) return trajectory?
 ##' 
 ##' @importFrom deSolve lsodar
 ##' @importFrom utils tail
 x_in_exact_scalar <- function(R0, epsilon, I0=1e-6,
+                              t_int = NULL,
                               tmin = 20,
-                              tmax = NULL,
+                              maxit = 10,
                               nt = 100,
                               return_traj = FALSE) {
 
     ## reasonable guess at an approximate max time for a specified R0
     ## (given default I0)
-    if (is.null(tmax)) tmax <- max(tmin, 100/(R0-1))
+    if (is.null(t_int)) t_int <- max(100/(R0-1), tmin)
 
     ## precompute useful factors, as well as hard-coding vars[i]
     ## lookup below rather than using with(as.list(...)), for speed
@@ -53,19 +55,28 @@ x_in_exact_scalar <- function(R0, epsilon, I0=1e-6,
         return(c(Ieqm, Ieqm2))
     }
 
-    ..nroot <- 0
-    dd <- deSolve::lsodar(y = c(S=1-I0, logI = log(I0)),
-                          ## ?? what should length.out be?
-                          times = seq(0, tmax, length.out = nt),
-                          func = sirgrad,
-                          parms = list(R0 = R0, epsilon = epsilon),
-                          rootfunc = rfunc,
-                          events = list(func = eventfun, root = TRUE,
-                                        ## terminate on criterion 2 (-> I==Ihat for second time)
-                                        terminalroot = 2))
+    ok <- FALSE
 
+    it <- 0
+    
+    while (!ok && it < maxit) {
+        ..nroot <- 0
+        dd <- deSolve::lsodar(y = c(S=1-I0, logI = log(I0)),
+                              ## ?? what should length.out be?
+                              times = seq(0, t_int, length.out = nt),
+                              func = sirgrad,
+                              parms = list(R0 = R0, epsilon = epsilon),
+                              rootfunc = rfunc,
+                              events = list(func = eventfun, root = TRUE,
+                                            ## terminate on criterion 2 (-> I==Ihat for second time)
+                                            terminalroot = 2))
+        ok <- max(dd[, "time"] < t_int)
+        it <- it + 1
+    }
+    
     if (return_traj) return(dd)
-    if (max(dd[, "time"]) == tmax) stop("reached maximum time")
+    if (max(dd[, "time"]) == t_int && it == maxit) stop(sprintf("reached maximum time (%1.1f): increase maxit?",
+                                                               t_int))
     tail(dd[,"S"], 1)
 }
 
